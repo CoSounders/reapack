a65882392dd42f7d41d79b8da6628f4ee6ca708f
waldek <waldek@mailbox.org>
2020-12-02 14:13:17 +0100

    and again

---
+++ pack_move_to_bus/metapackage.py
@@ -0,0 +1,7 @@
+"""
+@version 0.1.3
+@metapackage true
+@provides
+	[nomain] ./lib/lib_move_to_bus.py > lib_move_to_bus.py
+	[main] ./lib/move_selected_items_to_dialog_bus.py > move_selected_items_to_dialog_bus.py
+"""
