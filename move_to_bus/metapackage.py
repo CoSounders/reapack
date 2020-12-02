cd812c29286c88fecc7561a17f94d31f77f0f638
waldek <waldek@mailbox.org>
2020-12-02 16:25:50 +0100

    and again

--- move_to_bus/metapackage.py
+++ move_to_bus/metapackage.py
@@ -2,11 +2,8 @@
 @description Move to bus - metapackage
 @about
   # Metapackage: Move to bus
-
   This package installs all the *move to bus* scripts.
-
   [More information](https://86thumbs.mooo.com/)
-
 @version 0.1.3
 @author Wouter Gordts
 @metapackage true
