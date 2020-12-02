b290aba29c992c4b310f70758d56924c670f9039
waldek <waldek@mailbox.org>
2020-12-02 14:34:56 +0100

    and again

---
+++ move_to_bus/metapackage.py
@@ -0,0 +1,9 @@
+"""
+@version 0.1.3
+@author Wouter Gordts
+@metapackage true
+@provides
+	[nomain] ./lib_move_to_bus.py > lib_move_to_bus.py
+	[main] ./move_selected_items_to_dialog_bus.py > move_selected_items_to_dialog_bus.py
+	[main] ./move_selected_items_to_music_bus.py > move_selected_items_to_music_bus.py
+"""
---
+++ move_to_bus/move_selected_items_to_music_bus.py
@@ -0,0 +1,20 @@
+"""
+@noindex true
+"""
+
+from sys import stdout
+from logging import root, Formatter, StreamHandler, INFO, DEBUG, ERROR, CRITICAL
+from lib_move_to_bus import ReaperTrack, main
+
+LOGGER = root
+FMT_STR = '%(asctime)s - %(levelname)s - %(name)s.%(funcName)s %(message)s'
+DATE_STR = '%Y-%m-%d %H:%M:%S'
+FMT = Formatter(FMT_STR, DATE_STR)
+ST_HANDLER = StreamHandler(stdout)
+ST_HANDLER.setFormatter(FMT)
+LOGGER.addHandler(ST_HANDLER)
+LOGGER.setLevel(DEBUG)
+#LOGGER.setLevel(ERROR)
+
+ReaperTrack.NAME = "#MUSIC"
+main()
