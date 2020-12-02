dd4f859b2ba4bb22d7dd80b94499f0458109b88c
waldek <waldek@mailbox.org>
2020-12-02 12:55:01 +0100

    lets go

--- pack_move_to_bus/test_package.py
+++
@@ -1,7 +0,0 @@
-"""
-@description Move selected items to dialog bus package
-@author Wouter Gordts
-@version 0.2
-@metapackage true
-@provides [nomain] ../lib/lib_move_to_bus.py > lib_move_to_bus.py
-"""
