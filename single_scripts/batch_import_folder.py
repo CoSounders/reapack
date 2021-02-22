82705de604343f797e8f9b9765970cbc75be02c2
waldek <waldek@mailbox.org>
2021-02-22 16:29:53 +0100

    some general fixes

--- single_scripts/batch_import_folder.py
+++ single_scripts/batch_import_folder.py
@@ -14,14 +14,23 @@ def _get_source_folder():
     filename = str()
     title = "Paste the path to the source directory"
     num_inputs = 1
-    captions_csv = "path:"
+    captions_csv = "paste path to asset directory:, extrawidth=400"
     retvals_csv = str()
     retvals_csv_sz = 4096
     retval, title, num_inputs, captions_csv, retvals_csv, retvals_csv_sz = RPR_GetUserInputs(title, num_inputs, captions_csv, retvals_csv, retvals_csv_sz)
     if retval:
-        retvals_csv = "/home/waldek/Downloads/demute_hirez/Fenrir (Dog Armor) - PICKED/Fenrir (Dog Armor)"
-        path = pathlib.Path(retvals_csv)
-        return path
+        try:
+            #retvals_csv = "/home/waldek/Downloads/demute_hirez/Fenrir (Dog Armor) - PICKED/Fenrir (Dog Armor)"
+            path = pathlib.Path(retvals_csv)
+            if path.exists():
+                return path
+            else:
+                RPR_ShowConsoleMsg("Not a valid path!")
+                return False
+        except:
+            return False
+    else:
+        return False
 
 def _get_all_audio_files_from_folder(folderpath):
     all_audio_files = []
@@ -48,11 +57,9 @@ def _import_audiofile_into_session(filepath, regionname):
     cursor = RPR_GetCursorPosition()
     track = RPR_GetTrack(0, 0)
     count = RPR_CountTrackMediaItems(track)
-    print(count)
     item = RPR_GetMediaItem(0, count - 1)
     position = RPR_GetMediaItemInfo_Value(item, "D_POSITION")
     length = RPR_GetMediaItemInfo_Value(item, "D_LENGTH")
-    print(position, length)
     end = position + length
     RPR_AddProjectMarker2(0, True, position, end, str(regionname).strip(".wav"), 0, 0)
     RPR_MoveEditCursor(2, False)
@@ -64,23 +71,15 @@ def _normalize_and_lower_volume():
     for i in range(0, 3):
         RPR_Main_OnCommand(41924, 0) # nudge item volume -1db
 
-def _ask_for_track_processing():
-    # TODO not sure how to do this
-    filename = str()
-    title = "Add FX chain to {}".format("tmp")
-    deftext = "RfxChain"
-    retval, filename, title, defext = RPR_GetUserFileNameForRead(filename, title, defext)
-
 
 if __name__ == "__main__":
     PATH = _get_source_folder()
-    ALL_AUDIO_FILES = _get_all_audio_files_from_folder(PATH)
-    RPR_Undo_BeginBlock()
-    _prepare_session()
-    for filepath, regionname in ALL_AUDIO_FILES:
-        _import_audiofile_into_session(filepath, regionname)
-    _normalize_and_lower_volume()
-    RPR_Undo_EndBlock(0, 0)
-    RPR_UpdateArrange()
-
-
+    if PATH is not False:
+        ALL_AUDIO_FILES = _get_all_audio_files_from_folder(PATH)
+        RPR_Undo_BeginBlock()
+        _prepare_session()
+        for filepath, regionname in ALL_AUDIO_FILES:
+            _import_audiofile_into_session(filepath, regionname)
+        _normalize_and_lower_volume()
+        RPR_Undo_EndBlock(0, 0)
+        RPR_UpdateArrange()
